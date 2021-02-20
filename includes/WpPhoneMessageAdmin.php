96c68387925a962d9142db8c568d17fba1b87b28
webmarcello8080 <webmarcello8080@gmail.com>
2021-02-20 13:34:48 +0000

    freemium menu items

--- includes/WpPhoneMessageAdmin.php
+++ includes/WpPhoneMessageAdmin.php
@@ -7,6 +7,8 @@ if ( !class_exists( 'WpPhoneMessageAdmin' ) ) {
 
         public function __construct(){
             add_action( 'admin_menu', array( $this, 'adminMenu' ) );
+            add_action( 'admin_menu', array( $this, 'freemiusMenu' ) );
+            do_action( 'wpm_fs_loaded' );
             add_action( 'admin_post', array( $this, 'adminSave' ) );
             add_action( 'admin_enqueue_scripts', array( $this, 'adminStyle' ) );
             $this->model = new WpPhoneMessageModel;
@@ -21,6 +23,40 @@ if ( !class_exists( 'WpPhoneMessageAdmin' ) ) {
                 array( $this, 'adminPage' ),
                 91
             );
+        }
+
+        public function freemiusMenu(){
+            global $wpm_fs;
+
+            if ( ! isset( $wpm_fs ) ) {
+                // Include Freemius SDK.
+                require_once PLUGIN_WPM_PATH . 'freemius/start.php';
+
+                $wpm_fs = fs_dynamic_init( array(
+                    'id'                  => '7845',
+                    'slug'                => 'wp-phone-message',
+                    'type'                => 'plugin',
+                    'public_key'          => 'pk_96faea554c5400ff00a15b200fd41',
+                    'is_premium'          => true,
+                    'premium_suffix'      => 'Premium',
+                    // If your plugin is a serviceware, set this option to false.
+                    'has_premium_version' => true,
+                    'has_addons'          => false,
+                    'has_paid_plans'      => true,
+                    'menu'                => array(
+                        'slug'           => 'wp-phone-message-admin',
+                        'support'        => false,
+                        'parent'         => array(
+                            'slug' => 'options-general.php',
+                        ),
+                    ),
+                    // Set the SDK to work in a sandbox mode (for development & testing).
+                    // IMPORTANT: MAKE SURE TO REMOVE SECRET KEY BEFORE DEPLOYMENT.
+                    'secret_key'          => 'sk_QJm%p>es2*s^D5$>_i)@ae[7W*-jz',
+                ) );
+            }
+
+            return $wpm_fs;
         }
 
         public function adminPage() {
