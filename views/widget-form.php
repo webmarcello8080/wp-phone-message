33696751e0fae07e0c1a9104cea31883ad257693
webmarcello8080 <webmarcello8080@gmail.com>
2021-02-20 16:06:58 +0000

    widget is now indipendent from shortcode

--- views/widget-form.php
+++ views/widget-form.php
@@ -3,20 +3,20 @@
         <p class="whatapp-text"><?= $text; ?></p>
         <?php
         if(get_option('wp-phone-message-name-active-widget')){
-            echo '<input type="text" class="wp-phone-message-name" id="wp-phone-message-widget-name" placeholder="' . get_option('wp-phone-message-name') . '"  ' . get_option('wp-phone-message-name-mandatory') . ' />';
+            echo '<input type="text" class="wp-phone-message-name" id="wp-phone-message-widget-name" placeholder="' . get_option('wp-phone-message-name-widget') . '"  ' . get_option('wp-phone-message-name-mandatory-widget') . ' />';
         }
         if(get_option('wp-phone-message-address-active-widget')){
-            echo '<input type="text" class="wp-phone-message-address" id="wp-phone-message-widget-address" placeholder="' . get_option('wp-phone-message-address') . '"  ' . get_option('wp-phone-message-address-mandatory') . ' />';
+            echo '<input type="text" class="wp-phone-message-address" id="wp-phone-message-widget-address" placeholder="' . get_option('wp-phone-message-address-widget') . '"  ' . get_option('wp-phone-message-address-mandatory-widget') . ' />';
         }
         if(get_option('wp-phone-message-phone-active-widget')){
-            echo '<input type="text" class="wp-phone-message-phone" id="wp-phone-message-widget-phone" placeholder="' . get_option('wp-phone-message-phone') . '"  ' . get_option('wp-phone-message-phone-mandatory') . ' />';
+            echo '<input type="text" class="wp-phone-message-phone" id="wp-phone-message-widget-phone" placeholder="' . get_option('wp-phone-message-phone-widget') . '"  ' . get_option('wp-phone-message-phone-mandatory-widget') . ' />';
         }
         if(get_option('wp-phone-message-email-active-widget')){
-            echo '<input type="email" class="wp-phone-message-email" id="wp-phone-message-widget-email" placeholder="' . get_option('wp-phone-message-email') . '"  ' . get_option('wp-phone-message-email-mandatory') . ' />';
+            echo '<input type="email" class="wp-phone-message-email" id="wp-phone-message-widget-email" placeholder="' . get_option('wp-phone-message-email-widget') . '"  ' . get_option('wp-phone-message-email-mandatory-widget') . ' />';
         }
         ?>
-        <textarea class="wp-phone-message-message" id="wp-phone-message-widget-message" placeholder="<?= get_option('wp-phone-message-textarea'); ?>" required></textarea>
+        <textarea class="wp-phone-message-message" id="wp-phone-message-widget-message" placeholder="<?= get_option('wp-phone-message-textarea-widget'); ?>" required></textarea>
         <p class="whatapp-error" id="whatapp-widget-error" ></p>
-        <input type="submit" class="wp-phone-message-button" id="wp-phone-message-widget-button" value="<?= get_option('wp-phone-message-button'); ?>" />
+        <input type="submit" class="wp-phone-message-button" id="wp-phone-message-widget-button" value="<?= get_option('wp-phone-message-button-widget'); ?>" />
     </form>
 </div>
