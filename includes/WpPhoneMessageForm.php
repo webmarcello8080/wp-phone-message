f31b1204e50ca3c2ba2ae612df1419940acf54e5
webmarcello8080 <webmarcello8080@gmail.com>
2021-02-21 11:58:42 +0000

    display error message only for free users

--- includes/WpPhoneMessageForm.php
+++ includes/WpPhoneMessageForm.php
@@ -4,11 +4,15 @@ if ( !class_exists( 'WpPhoneMessageForm' ) ) {
 
    class WpPhoneMessageForm {
       
-   public static function isPremiumOnly() {
-      ?>
-      <p class="description form-messages" >This Field is available only for Premium customers.</p>
-      <?php
-   }
+      public static function isPremiumOnly() {
+         if ( wp_phone_message_wpm_fs()->is__premium_only() ) {
+            if(!wp_phone_message_wpm_fs()->is_plan('premium')){
+               ?>
+               <p class="description form-messages" >This Field is available only for Premium customers.</p>
+               <?php
+            }
+         }
+      }
 
    }
 }
